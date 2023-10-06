88aafd8af8be0a8b91ae9a2f075bae73dec3d1e6
Mario Dickerson <dai.dickerson@gmail.com>
2023-10-05 19:45:54 -0500

    running test

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -7,6 +7,7 @@ public class PositionType extends JobField{
 
     public PositionType(String value) {
         super(value);
+
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
